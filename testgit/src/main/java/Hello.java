0f0ca2179ab4868e9e2bb65c2402fe57291a072d
lavender <1350508231@qq.com>
2020-09-08 09:52:30 +0800

    本地工程添加到git

---
+++ testgit/src/main/java/Hello.java
@@ -0,0 +1,14 @@
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @Auther: 赵岱麒
+ * @Date: 2020/09/08/9:32
+ * @Description: 测试git
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("测试git");
+        System.out.println("测试git");
+        System.out.println("测试git");
+    }
+}
